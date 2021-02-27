bacaddf76b04b86fa1c85c7947b878f54d0e93f5
wangtingkeng <290669132@qq.com>
2021-02-27 16:41:40 +0800

    feat: 路径和23

--- 2.二叉树/113. 路径总和2.js
+++ 2.二叉树/113. 路径总和2.js
@@ -34,10 +34,51 @@ var tree = {
  * }
  */
 /**
+ * path.pop剪枝操作比较有用，需要学会使用
+ * 思路：
+ * 1递归且记录路径paths
+ * 2是叶子节点 且 paths 总和为目标值 =》 推入answer
+ * 
+ * 收获：
+ * 1.可以只使用一个path数组来记录路径以节省内存开销；
+ * 但要注意：
+ * 1任何一个节点处理完成时，都要把当前节点 pop 出 path 数组；
+ * 2注意最终push(path.slice())这里要浅拷贝一下，防止下面的计算污染这个数组
  * @param {TreeNode} root
  * @param {number} targetSum
  * @return {boolean}
  */
-var hasPathSum = function(root, targetSum) {
-    // path.pop剪枝操作比较有用，需要学会使用
+var pathSum = function(root, targetSum) {
+  const answer = []
+  function search(root, path) {
+    if (isInvalid(node)) return;
+
+    path.push(root.val)
+
+    if (!root.left && !root.right) {
+      if (isEqualTargetSum(path, targetSum)) {
+        answer.push(path.slice())
+      }
+    }
+
+    if (root.left) {
+      search(root.left, path)
+    }
+    if (root.right) {
+      search(root.right, path)
+    }
+    path.pop()
+  }
+  search(root, [])
+  return answer
 };
+
+function isEqualTargetSum(paths, sum) {
+  return paths.reduce((accumulator, currentValue)=>{
+    return accumulator + currentValue
+  }) === sum
+}
+
+function isInvalid(node) {
+  return !node || node.val === undefined || node.val === null;
+}
--- 2.二叉树/437. 路径总和3.js
+++ 2.二叉树/437. 路径总和3.js
@@ -34,22 +34,29 @@ var tree = {
  * }
  */
 /**
+ * 思路：
+ * 1.借鉴路径总和2中的思路，记录下path
+ * 2.定义个函数来计算每个节点的path中有几种情况是符合要求的
  * 
+ * 优点：思路直观；缺点：开销大，慢；
+ * 执行用时：1140 ms, 在所有 JavaScript 提交中击败了5.10%的用户
+ * 内存消耗：44.8 MB, 在所有 JavaScript 提交中击败了11.04%的用户
+ * 
+ * TODO: 
+ * 1可以优化成只记录所有路径的节点和吗？
  * @param {TreeNode} root
  * @param {number} targetSum
  * @return {boolean}
  */
 var pathSum = function(root, targetSum) {
-  const answer = []
+  let answer = 0
   function search(root, path) {
     if (isInvalid(root)) return;
 
     path.push(root.val)
 
-    const sumPath = getSumPath(path, targetSum)
-      if (sumPath.length) {
-        answer.concat(sumPath)
-      }
+    const pathNum = getPathNum(path, targetSum)
+    answer += pathNum
 
     if (root.left) {
       search(root.left, path)
@@ -63,20 +70,34 @@ var pathSum = function(root, targetSum) {
   return answer
 };
 
-function getSumPath(paths, sum) {
-  const len = paths.length
-  const targetPath = []
+/**
+ * 计算每个节点的paths中符合要求的路径有几个
+ * @param {*} paths 
+ * @param {*} sum 
+ */
+function getPathNum(paths, sum) {
+  let num = 0
   for (let i = 0; i < paths.length; i++) {
     const res = paths.slice(i).reduce((accumulator, currentValue)=>{
       return accumulator + currentValue
     })
     if (res === sum) {
-      targetPath.push(paths.slice(i))
+      num++
     }
   }
-  return targetPath
+  return num
 }
 
+// 判断传入的node是否为有效节点
 function isInvalid(node) {
   return !node || node.val === undefined || node.val === null;
 }
+
+/**
+ * TODO: 
+ * 解法二：双递归思路：
+ * 第一个递归：用于遍历每个结点（pathSum）
+ * 第二个递归：从该节点开始向下找存在的路径个数（countSum）
+ * 答案见https://github.com/sl1673495/leetcode-javascript/issues/61
+ * 因为没有类似题目类比理解，先跳过，以后遇到类似再回来理解
+ */
