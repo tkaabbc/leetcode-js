cfbc03ac5c6869e452136b18e0308b14701fc108
Tingkeng Wang <wangtingkeng@kezaihui.com>
2021-08-16 19:57:36 +0800

    feat: 背包例题

---
+++ 7.DP/背包问题例题/todo.js
@@ -0,0 +1,7 @@
+/**
+ * todo 找到各种类别的背包问题例题
+ * https://blog.csdn.net/whc__/article/details/118308386?utm_medium=distribute.pc_relevant.none-task-blog-2~default~baidujs_title~default-0.control&spm=1001.2101.3001.4242
+ * https://zhuanlan.zhihu.com/p/93857890
+ * https://blog.csdn.net/HangHug_L/article/details/108491601?utm_medium=distribute.pc_relevant.none-task-blog-2~default~baidujs_title~default-0.control&spm=1001.2101.3001.4242
+ * 注：动态规划问题因为很多没有套路，先学有套路的背包问题，剩下的量力而行
+ */
