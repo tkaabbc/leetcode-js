cce2542cf3e5ae300ea1c5ce374304d6abf7e88e
wangtingkeng <290669132@qq.com>
2021-12-27 00:27:27 +0800

    fix: 注释补充

--- 7.DP/70爬楼梯.js
+++ 7.DP/70爬楼梯.js
@@ -62,7 +62,7 @@ var climbStairs = function (n) {
  */
 var climbStairs = function (n) {
   const dp = []
-  dp[0] = 1
+  dp[0] = 1 // 哨兵节点 从dp[2] 为2 推出
   dp[1] = 1
   for (let i = 2; i <= n; i++) {
     dp[i] = dp[i - 1] + dp[i -2 ]
