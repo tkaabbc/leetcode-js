14dd2959560bbc391ded791b82d86178d4a1fd80
wangtingkeng <290669132@qq.com>
2022-01-23 23:54:08 +0800

    feat: 补充题目描述

--- 其它经典/504.七进制数.js
+++ 其它经典/504.七进制数.js
@@ -1,3 +1,28 @@
+/**
+ * 
+给定一个整数 num，将其转化为 7 进制，并以字符串形式输出。
+
+ 
+
+示例 1:
+
+输入: num = 100
+输出: "202"
+示例 2:
+
+输入: num = -7
+输出: "-10"
+ 
+
+提示：
+
+-107 <= num <= 107
+
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/base-7
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
 var convertToBase7 = function(num) {
   let ret = '', flag = false
   if (num < 0) {
